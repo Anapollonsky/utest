e1b9725725d0101749470a3adf3e90d883ac487c
Andrew Apollonsky <Anapollonsky@gmail.com>
2015-03-17 01:08:21 -0500

    First test, first maybe-functioning mxa scpi frame, mxa test.

--- gut/interfaces/MXA_Frame.py
+++ gut/interfaces/MXA_Frame.py
@@ -3,7 +3,7 @@ import telnetlib
 import socket
 from interfaces.scpi_frame import scpi_Frame
 from decorators import command
-
+  
 class MXA_Frame(scpi_Frame):
     interfacename = "mxa"
     
@@ -16,10 +16,51 @@ class MXA_Frame(scpi_Frame):
         time.sleep(.2)
         return con
 
+    def send_string(self, string):
+        """ Format a string for proper sendoff. """
+        self._connection.write((string + "\n").encode('ascii'))
+    
+    def marker_mode(ind, mode):
+        """Set marker mode. Ind is value 1-12, mode is one of 'POS', 'DELT', 'FIX', 'OFF'"""
+        if ind in range(1, 13):
+            self.end_string("CALC:MARK" + str(ind) + ":MODE " + mode.upper())
+        else:
+            self.conman.ferror("Invalid parameters passed to " + __func__ ": " + str(ind)+ " | " + str(mode))
+
+    def marker_axis_set(ind, axis, value)
+        """ Set marker axis value """
+        axes = ['x','y','z']
+        if axis in axes and ind in range(1, 13):
+            self.send_string("CALC:MARK" + str(ind) + ":" + axis.upper() + " " + value) 
+        else:
+            self.conman.ferror("Invalid parameters passed to " + __func__ ": " + str(ind)+ " | " + str(axis))
+
+    def marker_axis_get(ind, axis)
+        """ Get marker axis value """
+        axes = ['x','y','z']
+        if axis in axes and ind in range(1, 13):
+            self.send_string("CALC:MARK" + str(ind) + ":" + axis.upper() + "?") 
+        else:
+            self.conman.ferror("Invalid parameters passed to " + __func__ ": " + str(ind)+ " | " + str(axis))
+            
+        
 ################################################################################
 #################### Command functions
 
     @command(3)        
     def center_freq(self, freq, unit="MHz"):
-        """Permanently Set center frequency, in MHz by default"""
-        self._connection.write((":SENS:FREQ:CENT " + str(freq) + " " + str(unit) + "\n").encode('ascii'))
+        """Permanently Set center frequency, in MHz by default. Must be set in vicinity of"""
+        self.send_string(":SENS:FREQ:CENT " + str(freq) + " " + str(unit))
+
+    @command(4)
+    def send_val_freq(self, freq, axis = 'y', marker = 12):
+        """Get value at frequency"""
+        marker_axis_set(marker, 'x', freq)
+        marker_axis_get(marker, axis)
+
+    @command(4)
+    def send_find_peaks(self, source = 1, threshold = 10, excursion = -200, sort = "FREQ"):
+        """Provide list of Amplitude,Frequency pairs for given threshold and excurion"""
+        self.end_string(":CALC:DATA" + str(source) + ":PEAK? " + excursion + "," + threshold + "," + sort)        
+
+    
---
+++ gut/tests/test_frame.py
@@ -0,0 +1,9 @@
+import pytest
+from ../frame.py import Frame
+from ../frame.py import Interactive_Frame
+
+basic = {"interface": "sh", "send":, 
+
+
+def test_frame_composition():
+    
