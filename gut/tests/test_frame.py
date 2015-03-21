18195a4f80aa4d66978b643e09723b29b0bd7968
Andrew Apollonsky <Anapollonsky@gmail.com>
2015-03-20 23:13:15 -0500

    Playing with pytest.

--- gut/tests/test_frame.py
+++ gut/tests/test_frame.py
@@ -1,9 +1,15 @@
 import pytest
-from ../frame.py import Frame
-from ../frame.py import Interactive_Frame
+from interfaces.frame import Frame
+from interfaces.frame import Interactive_Frame
+from conman import Conman
 
-basic = {"interface": "sh", "send":, 
+basic = {"interface": "sh", "send": "ls /"}
 
+conman = Conman(3)
 
-def test_frame_composition():
-    
+def test_frame_composition(los, conman, interfacename):
+    frame = conman.get_interface(interfacename)(los, conman)
+    for member in los:
+        assert str(member) in frame.args
+
+test_frame_composition(basic, conman, "telnet")
